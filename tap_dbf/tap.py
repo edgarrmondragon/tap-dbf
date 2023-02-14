9d81920c5f8c80d89d65d2fca0a2e973bea941f2
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-02-14 13:27:47 -0600

    chore: Update dependencies

--- tap_dbf/tap.py
+++ tap_dbf/tap.py
@@ -1,28 +1,37 @@
 """Singer SDK classes."""
 
-import builtins
-from os import PathLike
-from pathlib import Path
-from typing import Any, Dict, Iterable, List, Optional
+from __future__ import annotations
 
-from dbfread import DBF  # type: ignore
-from dbfread.dbf import DBFField  # type: ignore
-from fs.base import FS
+import builtins
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable
+
+from dbfread import DBF
 from fs.opener import parse, registry
-from singer_sdk import Stream, Tap  # type: ignore
-from singer_sdk.typing import (  # type: ignore
-    BooleanType,
-    PropertiesList,
-    Property,
-    StringType,
-)
+from singer_sdk import Stream, Tap
+from singer_sdk.typing import BooleanType, PropertiesList, Property, StringType
+
+if TYPE_CHECKING:
+    from os import PathLike
+    from types import TracebackType
+
+    from dbfread.dbf import DBFField
+    from fs.base import FS
+
 
 RawRecord = Dict[str, Any]
 
 
-def dbf_field_to_jsonschema(field: DBFField) -> Dict[str, Any]:
-    """Map a .dbf data type to a JSON schema."""
-    d: Dict[str, Any] = {}
+def dbf_field_to_jsonschema(field: DBFField) -> dict[str, Any]:
+    """Map a .dbf data type to a JSON schema.
+
+    Args:
+        field: The field to map.
+
+    Returns:
+        A JSON schema.
+    """
+    d: dict[str, Any] = {}
     d["type"] = ["null"]
 
     if field.type == "N":
@@ -48,24 +57,50 @@ def dbf_field_to_jsonschema(field: DBFField) -> Dict[str, Any]:
     return d
 
 
-class patch_open:
+class PatchOpen:
     """Context helper to patch the builtin open function."""
 
-    def __init__(self, fs: FS):
-        """Patch builtins.open with a custom open function."""
+    def __init__(self: PatchOpen, fs: FS) -> None:
+        """Patch builtins.open with a custom open function.
+
+        Args:
+            fs: The filesystem instance to use.
+        """
         self.old_impl = _patch_open(fs.open)
 
-    def __enter__(self):
-        """Create a context for the patched function."""
+    def __enter__(self: PatchOpen) -> PatchOpen:
+        """Create a context for the patched function.
+
+        Returns:
+            The PatchOpen context.
+        """
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """Exit context and revert patch."""
+    def __exit__(
+        self: PatchOpen,
+        exc_type: type[Exception],
+        exc_val: Exception,
+        exc_tb: TracebackType,
+    ) -> None:
+        """Exit context and revert patch.
+
+        Args:
+            exc_type: The exception type.
+            exc_val: The exception value.
+            exc_tb: The exception traceback.
+        """
         _patch_open(self.old_impl)
 
 
-def _patch_open(func):
-    """Patch `builtins.open` with `func`."""
+def _patch_open(func: Callable) -> Callable:
+    """Patch `builtins.open` with `func`.
+
+    Args:
+        func: The function to patch `builtins.open` with.
+
+    Returns:
+        The original `builtins.open` function.
+    """
     old_impl = builtins.open
     builtins.open = func
     return old_impl
@@ -77,18 +112,26 @@ class DBFStream(Stream):
     primary_keys = ["_sdc_filepath", "_sdc_row_index"]
 
     def __init__(
-        self,
+        self: DBFStream,
         tap: Tap,
         filepath: PathLike,
         filesystem: FS,
+        *,
         ignore_missing_memofile: bool = False,
-    ):
-        """Create a new .DBF file stream."""
+    ) -> None:
+        """Create a new .DBF file stream.
+
+        Args:
+            tap: The tap instance.
+            filepath: The path to the .DBF file.
+            filesystem: The filesystem instance to use.
+            ignore_missing_memofile: Whether to ignore missing .DBT files.
+        """
         self.filepath = filepath
         self.filesystem = filesystem
         name = Path(filepath).stem
 
-        with patch_open(filesystem):
+        with PatchOpen(filesystem):
             self._table = DBF(
                 filepath,
                 ignorecase=False,
@@ -107,9 +150,12 @@ class DBFStream(Stream):
 
         super().__init__(tap, schema=schema, name=name)
 
-    def get_records(self, context: Optional[dict] = None) -> Iterable[RawRecord]:
-        """Get .DBF rows."""
-        # with patch_open(self.filesystem):
+    def get_records(self: DBFStream, _: dict | None = None) -> Iterable[RawRecord]:
+        """Get .DBF rows.
+
+        Yields:
+            A row of data.
+        """
         for index, row in enumerate(self._table):
             row["_sdc_filepath"] = self.filepath
             row["_sdc_row_index"] = index
@@ -126,14 +172,24 @@ class TapDBF(Tap):
         Property("ignore_missing_memofile", BooleanType, default=False),
     ).to_dict()
 
-    def discover_streams(self) -> List[DBFStream]:
-        """Discover .DBF files in the path."""
+    def discover_streams(self: TapDBF) -> list[DBFStream]:
+        """Discover .DBF files in the path.
+
+        Returns:
+            A list of streams.
+        """
         streams = []
 
         root = self.config["fs_root"]
         parse_result = parse(root)
         opener = registry.get_opener(parse_result.protocol)
-        filesystem = opener.open_fs(root, parse_result, False, False, "")
+        filesystem = opener.open_fs(
+            root,
+            parse_result,
+            writeable=False,
+            create=False,
+            cwd="",
+        )
 
         for match in filesystem.glob(self.config["path"]):
             path = match.path

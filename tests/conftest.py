df6c182fda26237874f4545606e57c99c4222695
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-06-14 13:26:20 -0600

    chore: Use Ruff exclusively

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Pytest configuration for the tap-dbf tests."""
+from __future__ import annotations
 
 # register the singer_sdk pytest plugin
 pytest_plugins = ("singer_sdk.testing.pytest_plugin",)
